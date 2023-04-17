8c572c252e88c5da0dbf97b5974f5166327cb056
YunjiePeng <67147416+YunjiePeng@users.noreply.github.com>
2023-04-17 14:53:09 +0800

    Update test.sh

--- spaAlignTemOccRecover/test.sh
+++ spaAlignTemOccRecover/test.sh
@@ -11,10 +11,10 @@
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --master_port=1123 --nproc_per_node=8 lib/main.py --cfgs ./config/baseline3d.yaml --phase test
 
 # # SpatialTemporalBackbone (STBackbone)
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --master_port=1123 lib/main.py --cfgs ./config/stbackbone.yaml --iter 40000 --phase test
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --master_port=1123 lib/main.py --cfgs ./config/stbackbone.yaml --iter 40000 --phase test
 
 # # STBackbone+SpaAlign
 # CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --master_port=1123 lib/main.py --cfgs ./config/spaAlign.yaml --iter 40000 --phase test
 
 # # SpaAlignTemOccRecover
-# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --master_port=1123 lib/main.py --cfgs ./config/spaAlignTemOccRecover.yaml --iter 40000 --phase test
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=8 --master_port=1123 lib/main.py --cfgs ./config/spaAlignTemOccRecover.yaml --iter 40000 --phase test
